1fa5f441c49cd67fe92fdd2059fce83a58588826
autoscatto <4utoscatto@gmail.com>
2014-10-03 22:53:40 +0200

    when the bamba then imbianca

---
+++ js/main.js
@@ -0,0 +1,39 @@
+(function() {
+  var httpRequest;
+  function makeRequest(url) {
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+      httpRequest = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE
+      try {
+        httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+      } 
+      catch (e) {
+        try {
+          httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+        } 
+        catch (e) {}
+      }
+    }
+    if (!httpRequest) {return false;}
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', url);
+    httpRequest.setRequestHeader ("Authorization", "Client-ID cc01e3195c1adc2");
+    httpRequest.send();
+  }
+  function alertContents() {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        var respa =JSON.parse(httpRequest.responseText);
+        var photosect = document.getElementById('photos');
+        for (x in respa.data ){  
+                var img = document.createElement("img");
+                img.src = respa.data[x].link;
+                photosect.appendChild(img);
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+  makeRequest('https://api.imgur.com/3/gallery/r/nsfw/');
+})();
