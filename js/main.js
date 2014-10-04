f1b73b88291e93001e02d7182f972ef0827a6732
leonardo <leofiore@libero.it>
2014-10-04 22:08:38 +0200

    Director, preloads, fades and many more useless things
    moar to come
    inoltre non capisco perché committo in inglese.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
 (function() {
+  "use strict";
   var httpRequest,
-      getById = document.getElementById;
+      evt_status = 0,
+      evt_complete = 273, //111
+      evt_load_music = 1, //001
+      evt_load_pic = 16,  //010
+      evt_load_xhr = 256, //100
 
-  function makeRequest(url) {
+      loop = document.getElementById('loop'),
+      img = document.getElementById('heroimg');
+
+  loop.onloadeddata = function(){
+    director(evt_load_music);
+  };
+  img.onload = function(){
+    director(evt_load_pic);
+  };
+
+  img.src = 'images/andrea.png';
+
+  function director(event){
+    evt_status |= event;
+    console.log(evt_status);
+    if (evt_status == evt_complete){
+        play();
+    }
+  }
+
+  function makeRequest(url, cbk) {
     var factory = [
       [window.XMLHttpRequest, undefined],
       [window.ActiveXObject, "Msxml2.XMLHTTP"],
@@ -11,7 +36,7 @@
     for (var i = 0; i < factory.length; i++) {
       try {
         httpRequest = new factory[i][0](factory[i][1]);
-        httpRequest.onreadystatechange = alertContents;
+        httpRequest.onreadystatechange = cbk;
         httpRequest.open('GET', url);
         httpRequest.setRequestHeader ("Authorization", "Client-ID cc01e3195c1adc2");
         httpRequest.send();
@@ -23,16 +48,27 @@
   function alertContents() {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
-        var respa = JSON.parse(httpRequest.responseText);
-        var photosect = getById('photos');
-        for (var x in respa.data ){  
+        var respa = JSON.parse(httpRequest.responseText),
+            photosect = document.getElementById('photos');
+        for (var x in respa.data ){
                 var img = document.createElement("img");
                 img.src = respa.data[x].link;
                 photosect.appendChild(img);
         }
+        director(evt_load_xhr);
       }
     }
   }
 
-  makeRequest('https://api.imgur.com/3/gallery/r/nsfw/');
+
+  function play(){
+    loop.play(); 
+    img.style.opacity = 1;
+    window.setInterval(function(){
+        img.style.opacity = Math.min(Math.random() + 0.5, 1);
+    }, 10000);
+  }
+
+  makeRequest('https://api.imgur.com/3/gallery/r/nsfw/', alertContents);
+  //makeRequest('http://localhost:8000/test.json', alertContents);
 })();
